7b9189cbb059e47ab3b4afd9953cf07da52e9990
Adem Cooper <acooper@brightidea.com>
2025-02-08 22:44:46 -0800

    Working with groovy

---
+++ src/test/java/FirstGroovyScript.groovy
@@ -0,0 +1,15 @@
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstGroovyScript {
+    public static void main(String[] args) {
+        //System.setProperty("webdriver.chrome.driver", "/Users/Brightidea/Documents/chromedriver-mac-arm64/chromedriver");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com");
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+}
